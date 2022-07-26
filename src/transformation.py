4852edeb88da1868d8c5c1a23d8af9131224d4c1
Luba <luciano.eq@outlook.com>
2022-07-26 20:41:46 -0300

    refactor: change name.

--- src/transformation.py
+++ src/transformation.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def get_data():
     numbers = [7, 8, 9, 10, 11, 12]
-    persons = ["guilherme", "pedro", "fernando", "maria", "antônia", "joana"]
+    persons = ["Manuel", "pedro", "fernando", "maria", "antônia", "joana"]
 
     dict_to_df = {"names": persons, "numbers": numbers}
 
